4c086177526df321d3b1e0ec2960e4f25c92bc00
max <max@ya.ru>
2020-01-11 00:10:30 +0300

    Добавил описание к gulpfile

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Example-for-Sass-lessons-site-on-installing-and-configuring-SCSS-compiler
+
 Пример для сайта sass-lessons по установке и настройке компилятора SCSS-файлов
--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
-// Объявляем наши плагины
+// Выводим служебные функции gulp
 const  { src, dest, parallel, watch, series } = require('gulp');
+// Объявляем наши модули
 const sass = require('gulp-sass');
-const plumber = require('gulp-plumber');
 const  connect = require('gulp-connect');
 const autoprefixer = require('gulp-autoprefixer');
 const gcmq = require('gulp-group-css-media-queries');
 const cleanCSS = require('gulp-clean-css');
 sass.compiler = require('node-sass');
+
+// Настраиваем  локальный сервер, локальный сервер будет брать файлы из папки public, и отображать их по адресу http://localhost:8080/
 
 function serveTask(done) {
   connect.server({
@@ -17,6 +19,8 @@ function serveTask(done) {
     this.server.on('close', done)
   });
 }
+
+// Настраиваем задачу для компиляции стилей. Компилируем данный файл "scss/main.scss", в нем как правило подключаются остальные файлы. Приделаем туда автопрефиксер, группировку медиазапросов, минификацию файлов, и сделаем так, чтобы наша сборка показывала строку с ошибкой если мы ее допустим.
 
 function style() {
   return src('scss/main.scss')
@@ -34,12 +38,15 @@ function style() {
   .pipe(connect.reload());
 }
 
-// Просмотр файлов
+// Просмотр файлов, если в папке "scss" происходят какие либо изменения с файлами имеющие расширения ".scss" то выполняем задачу "style"
+
 function watcher() {
   watch('scss/*.scss', style);
   watch('scss/*/*.scss', style);
 }
 
 exports.style = style;
+
+// Задачи которые будут выполнены когда мы введем в консоли gulp. Будут скомпилированы стили, запущен watcher и локальный сервер. Все эти задачи будут запущены паралельно, независимо друг от друга.
 
 exports.default = parallel(style, watcher, serveTask);
